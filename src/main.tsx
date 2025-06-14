b5e8e94ec87a32fbfa6ae15eb58990ce9ced3367
phamhongphuctu <phamtu660@gmail.com>
2025-06-14 12:31:15 -0600

    loi gi roi

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./i18n"; // giữ nguyên dòng này
+import "./i18n"; 
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Categories from "./pages/Categories";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/:countryCode" element={<App />} />
-      
+      <Route path=":countryCode" element={<App />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   </BrowserRouter>
 );
