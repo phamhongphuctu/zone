9b21593d2368917e57b7d0157c73916bccc3eaa1
phamhongphuctu <phamtu660@gmail.com>
2025-06-15 12:22:26 -0600

    aloa aa

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,28 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Categories from "./pages/Categories";
 import Profile from "./pages/Profile";
+import Sell from "./pages/Sell";
 import "./i18n";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
-import Sell from "./pages/Sell";
-
-
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-      <Routes>
-  <Route path="/" element={<App />} />
-  <Route path="/:countryCode/*" element={<App />} />
-  <Route path="/categories" element={<Categories />} />
-  <Route path="/profile" element={<Profile />} />
-  <Route path="/sell" element={<Sell />} />
-</Routes>
-
+        <Routes>
+          {/* Quan trong: /:countryCode/* phai co dau * moi support nested route */}
+          <Route path="/:countryCode/*" element={<App />} />
+          <Route path="/" element={<App />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sell" element={<Sell />} />
+        </Routes>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
